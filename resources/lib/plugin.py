9fcf4ab9cf12c6f8bcc2945218e755ff18439113
Jesus Trujillo <elyeyus@gmail.com>
2020-12-10 20:52:40 -0800

    add date to capitulos and change list styles

--- resources/lib/plugin.py
+++ resources/lib/plugin.py
@@ -21,6 +21,15 @@ ADDON_NAME = xbmcaddon.Addon().getAddonInfo("name")
 
 CATEGORY_PROGRAMAS = "Programas"
 
+VIEW_TYPE_PLAIN_LIST = 50
+VIEW_TYPE_BIG_LIST = 51
+VIEW_TYPE_ICON = 500
+VIEW_TYPE_POSTER_LIST = 501
+VIEW_TYPE_FANART_LIST = 508
+VIEW_TYPE_MEDIA_INFO_1 = 504
+VIEW_TYPE_MEDIA_INFO_2 = 503
+VIEW_TYPE_MEDIA_INFO_3 = 515
+
 def get_arg(key, default=None):
     # type: (str, Any) -> Any
     """Get the argument value or default"""
@@ -50,6 +59,8 @@ def add_menu_item(method, label, args=None, art=None, info=None, directory=True)
 def index():
     # type: () -> None
     """Main menu"""
+    xbmc.executebuiltin('Container.SetViewMode(%d)' % VIEW_TYPE_ICON)
+    xbmc.executebuiltin('Container.SetViewMode(%d)' % VIEW_TYPE_ICON)
     #add_menu_item(ultimos, "Ultimos", art=ku.icon("programas.png"))  # Ultimos
     add_menu_item(programas, CATEGORY_PROGRAMAS, art=ku.icon("programas.png"))  # Programas    
     xbmcplugin.setPluginCategory(plugin.handle, ADDON_NAME)
@@ -63,16 +74,20 @@ def programas():
     category = get_arg("category", CATEGORY_PROGRAMAS)  # Programas
     if not href:
         # Listado de Programas menu
+        xbmc.executebuiltin('Container.SetViewMode(%d)' % VIEW_TYPE_ICON)
+        xbmc.executebuiltin('Container.SetViewMode(%d)' % VIEW_TYPE_ICON)
         ps = canalsur.get_programas()
         for programa in ps:
             logger.info("programas->ku.art = {}".format(programa.thumbnail()))
             add_menu_item(programas,
                             programa.nombre,
-                            info={"plot": programa.descripcion.encode("ascii","ignore")},
+                            info={"Plot": programa.descripcion.encode("ascii","ignore")},
                             args={"href": programa.id, "category": programa.nombre.encode("ascii","ignore")},
                             art=ku.art(programa.thumbnail()))
     else:
         # Lista de Capitulos del programa (href es programaId)
+        xbmc.executebuiltin('Container.SetViewMode(%d)' % VIEW_TYPE_PLAIN_LIST)
+        xbmc.executebuiltin('Container.SetViewMode(%d)' % VIEW_TYPE_PLAIN_LIST)
         capitulos = canalsur.get_capitulos(id=href)        
         for capitulo in capitulos:            
             add_menu_item(play_film,
@@ -84,13 +99,14 @@ def programas():
                               "fecha": capitulo.fecha
                           },
                           info={
-                              "plot": capitulo.capitulo.encode("ascii","ignore")
+                              "Plot": capitulo.capitulo.encode("ascii","ignore"),
+                              "Date": capitulo.fecha
                           },
                           art=ku.art(capitulo.tapa),
                           directory=False)
         xbmcplugin.setContent(plugin.handle, "capitulos")
         xbmcplugin.addSortMethod(plugin.handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
-        xbmcplugin.addSortMethod(plugin.handle, xbmcplugin.SORT_METHOD_VIDEO_YEAR)
+        xbmcplugin.addSortMethod(plugin.handle, xbmcplugin.SORT_METHOD_DATE)
     xbmcplugin.setPluginCategory(plugin.handle, category)
     xbmcplugin.endOfDirectory(plugin.handle)
 
